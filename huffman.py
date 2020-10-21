6b73aaaeec87690db956eb7cb57be5fe2a3ac3b2
Jeevitha <jeevitha@multicorewareinc.com>
2020-10-21 08:54:20 +0530

    Add testcases and requested changes

--- huffman.py
+++ huffman.py
@@ -2,6 +2,8 @@
 import sys
 import argparse
 import shutil
+from queue import PriorityQueue
+from collections import defaultdict
 
 encode_map={}
 
@@ -13,57 +15,61 @@ class Node:
 		self.key = key
 		self.value = value
 		self.code=None
+	# create encoded map using root node of tree
+	def Huffman_code(self):
+		global encode_map
+		if(self != None):
+			if self.left != None:
+				self.left.Huffman_code()
+			if self.code != None:
+				encode_map[self.key]=self.code
+			if self.right != None:
+				self.right.Huffman_code()
 
-# create encoded map using root node of tree
-def Huffman_code(root):
-	global encode_map
-	if(root != None):
-		Huffman_code(root.left)
-		if root.code != None:
-			encode_map[root.key]=root.code
-		Huffman_code(root.right)
+	# inorder_traversal of tree
+	def inorder_traversal(self, encoding, count):
+		if(self != None):
+			if self.left != None:
+				encoding.insert(count, 0)
+				self.left.inorder_traversal(encoding, count+1)
+			if self.right != None:
+				encoding.insert(count, 1)
+				self.right.inorder_traversal( encoding, count+1)
+			if self.left == None and self.right == None:
+				encode=str()
+				for itr in range(count):
+					encode += str(encoding[itr])
+				self.code=encode
 
-# inorder_traversal of tree
-def inorder_traversal(root, encoding, count):
-	if(root != None):
-		if root.left != None:
-			encoding.insert(count, 0)
-			inorder_traversal(root.left, encoding, count+1)
-		if root.right != None:
-			encoding.insert(count, 1)
-			inorder_traversal(root.right, encoding, count+1)
-		if root.left == None and root.right == None:
-			encode=str()
-			for itr in range(count):
-				encode += str(encoding[itr])
-			root.code=encode
+# count the frequency of character in file
+def get_frequencies(content):
+	charcounts = defaultdict(int)
+	for char in content:
+		charcounts[char] = charcounts[char]+  1
+		if not char:
+			break
+	return charcounts
+# create priority queue inorder to maintain sorted order
+def generate_PriorityQueue(myQueue, charcounts):
+		for char,count in charcounts.items():
+			root = Node(char, count)
+			myQueue.put((count, char, root))
 
 # encode the input files
 def encode(input_file, output_file):
 	print("encoding ", input_file, output_file)
 	file1 = open(input_file, 'r')
-	count = 0
-	charcounts = {}
-	# count the frequency of character in file
-	while 1:
-		char = file1.read(1)
-		if (not char in charcounts):
-			charcounts[char] = 0
-		charcounts[char]  =charcounts[char]+  1
-		if not char:
-			break
-	from queue import PriorityQueue
-	myQueue = PriorityQueue()
+	contents = file1.read()
+	charcounts = get_frequencies(contents)
+	file1.close()
 	# create priority queue inorder to maintain sorted order
-	for char,count in charcounts.items():
-		root = Node(char, count)
-		myQueue.put((count, char, root))
+	myQueue = PriorityQueue()
+	generate_PriorityQueue(myQueue, charcounts)
 	global encode_map
 	last_node = []
 	encoding = []
-	counter = 1
-	file1.close()
 	# iterate over priority queue for creating tree by taking first two elements in myQueue
+	counter = 1
 	while not myQueue.empty():
 		if counter == 1:
 			left = myQueue.get()
@@ -88,25 +94,14 @@ def encode(input_file, output_file):
 			if myQueue.qsize() != 0 :
 				myQueue.put((frequency, char, new_node))
 		counter = counter + 1
-	counter = 0
 	# Traversing over the root node of tree inorder to set the encode string for tree elements
-	inorder_traversal(last_node, encoding, counter)
-	Huffman_code(last_node)
-	file1 = open(input_file, 'r')
-	file_content = file1.read()
-	file1.close()
-	file2 = open(output_file, 'w+')
-	code = str()
-	encode = str()
-	# write the encoded input file data into output file
-	for line in file_content:
-		code += line
-		if code in encode_map.keys():
-			encode += encode_map[code]
-			code = str()
-	file2.write(encode)
-	file2.close()
-	file = open('encode_map.txt', 'w')
+	counter = 0
+	last_node.inorder_traversal(encoding, counter)
+	last_node.Huffman_code()
+	# write the encoded data to output file
+	write_outputfile(input_file,output_file,encode_map)
+	file = open(output_file, 'a+')
+	file.write("`")
 	for char,encode in encode_map.items():
 		if char != '':
 			file.write(str(char))
@@ -114,33 +109,44 @@ def encode(input_file, output_file):
 			file.write('`')
 	file.close()
 
+def write_outputfile(input_file,output_file,encode_map):
+	file1 = open(input_file, 'r')
+	file_content = file1.read()
+	file1.close()
+	file2 = open(output_file, 'w+')
+	char = str()
+	# write the encoded/decoded data to output file
+	for line in file_content:
+		char += line
+		if char in encode_map.keys():
+			file2.write(encode_map[char])
+			char = str()
+		if char == '`':
+			break
+	file2.close()
+
 # decode the input files
 def decode(input_file, output_file):
 	print("decoding ", input_file, output_file)
 	encode_map = {}
-	file1 = open('encode_map.txt', 'r')
+	file1 = open(input_file, 'r')
 	char_str = str()
-	# Read the encoded map from file
+	count = 0
+	# Read the encoded map from input file
 	while 1:
 		char = file1.read(1)
+		# separating the encode and map by special character
 		if char == '`':
-			encode_map[char_str[1:]] = char_str[0]
+			count = count + 1
+			if count != 1:
+				encode_map[char_str[1:]] = char_str[0]
 			char_str = str()
 		else:
 			char_str += char
 		if not char:
 			break
 	file1.close()
-	file1 = open(input_file, 'r')
-	file_content = file1.read()
-	file2 = open(output_file, 'w+')
-	key = str()
-	# write the decoded data to output file
-	for line in file_content:
-		key += line
-		if key in encode_map.keys():
-			file2.write(encode_map[key])
-			key = str()
+	write_outputfile(input_file,output_file,encode_map)
 
 def get_options(args=sys.argv[1:]):
 	parser = argparse.ArgumentParser(description="Huffman compression.")
--- test.py
+++ test.py
@@ -1,19 +1,32 @@
 # Test functions goes here
 import unittest
-
-from huffman import encode, decode
+import filecmp
+from queue import PriorityQueue
+from huffman import encode, decode, get_frequencies, generate_PriorityQueue
 
 class TestHuffman(unittest.TestCase):
-	# write all your tests here
-	# function name should be prefixed with 'test'
 
-	def test_encode(self):
-		encode("file.txt", "testt.huff")
-		assert True
+	def test_get_frequencies(self):
+		expected_output = {'h':1,'i':1}
+		actual_output=get_frequencies("hi")
+		self.assertDictEqual(expected_output, actual_output)
+		print("tested frequencies")
 
-	def test_decode(self):
-		decode("testt.huff", "file_.txt")
-		assert True
+	def test_generate_PriorityQueue(self):
+		charcount = {'h':5,'i':4}
+		myQueue = PriorityQueue()
+		generate_PriorityQueue(myQueue, charcount)
+		first = myQueue.get()
+		assert first[0] == 4
+		second = myQueue.get()
+		assert second[0] == 5
+		print("tested priority order is corect or not")
+
+	def test_encode_decode(self):
+		encode("story.txt", "story.huff")
+		decode("story.huff", "story_.txt")
+		assert(filecmp.cmp("story.txt","story_.txt"))
+		print("Decoded file tested successfully")
 
 if __name__ == '__main__':
 	unittest.main()
